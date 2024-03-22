b6effe27659d76cc8067df0909136c507432f461
LeonardoWrk <63686229+LeonardoWrk@users.noreply.github.com>
2024-03-21 21:21:22 -0300

    localstorage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,40 +1,44 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import type { PageServerLoad } from './$types';
 
-async function scrapeGoogleFinance(): Promise<void> {
-	try {
-		const response = await axios.get('https://www.google.com/finance/markets/currencies');
-		const $ = cheerio.load(response.data);
+export const load: PageServerLoad = async () => {
+	async function scrapeGoogleFinance() {
+		try {
+			const response = await axios.get('https://www.google.com/finance/markets/currencies');
+			const $ = cheerio.load(response.data);
 
-		// achar the div with class name 'hyO8N' and extract its content
-		const currencyData: string = $('.hyO8N').text();
+			// achar the div with class name 'hyO8N' and extract its content
+			const currencyData: string = $('.hyO8N').text();
 
-		// Split the data by lines
-		const lines: string[] = currencyData.trim().split('add_circle_outline');
+			// Split the data by lines
+			const lines: string[] = currencyData.trim().split('add_circle_outline');
 
-		// Initialize an empty object to store formatted pairs
-		const formattedPairs: { [key: string]: number } = {};
+			// Initialize an empty object to store formatted pairs
+			const formattedPairs: { [key: string]: number } = {};
 
-		// Iterate through each line
-		lines.forEach((line: string) => {
-			// Extract currency pair and rate
-			const match: RegExpMatchArray | null = line.match(/CCY([A-Z]{3}) \/ ([A-Z]{3})([\d.-]+)/);
+			// Iterate through each line
+			lines.forEach((line: string) => {
+				// Extract currency pair and rate
+				const match: RegExpMatchArray | null = line.match(/CCY([A-Z]{3}) \/ ([A-Z]{3})([\d.-]+)/);
 
-			if (match) {
-				const currency1: string = match[1];
-				const currency2: string = match[2];
-				const rate: number = parseFloat(match[3]);
-				formattedPairs[`${currency1}/${currency2}`] = rate;
+				if (match) {
+					const currency1: string = match[1];
+					const currency2: string = match[2];
+					const rate: number = parseFloat(match[3]);
+					formattedPairs[`${currency1}/${currency2}`] = rate;
+				}
+			});
+
+			// Output the formatted pairs
+			for (const pair in formattedPairs) {
+				console.log(`${pair}: ${formattedPairs[pair]}`);
 			}
-		});
-
-		// Output the formatted pairs
-		for (const pair in formattedPairs) {
-			console.log(`${pair}: ${formattedPairs[pair]}`);
+			return formattedPairs;
+		} catch (error) {
+			console.error('Error fetching data:', error);
 		}
-	} catch (error) {
-		console.error('Error fetching data:', error);
 	}
-}
 
-scrapeGoogleFinance();
+	return { scrap: await scrapeGoogleFinance() };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	console.log(data.scrap);
+	onMount(() => {
+		localStorage.setItem('coin', JSON.stringify(data.scrap));
+	});
+</script>
+
+<h1>{JSON.stringify(data.scrap)}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
